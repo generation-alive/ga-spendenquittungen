afca3b4b41a7010cfaa84a4d5e28c00225b703cd
Kai Wittmann <k.wittmann@cmp-germany.com>
2018-12-28 22:02:31 +0100

    initialize mdc-layout

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,25 @@
 <template>
   <div class="app">
-    <hello-world class="app__hello"
-      title="Welcome to your Vue MDC Adapter App"
-      :vmaLinks="vmaLinks"
-      :mdcLinks="mdcLinks">
-    </hello-world>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   data () {
-    return {
-      vmaLinks: [
-        {
-          title: 'Documentation',
-          url: 'https://stasson.github.io/vue-mdc-adapter'
-        },
-        {
-          title: 'GitHub',
-          url: 'https://github.com/stasson/vue-mdc-adapter'
-        },
-        {
-          title: 'Twitter',
-          url: 'https://twitter.com/vuemdc'
-        },
-        {
-          title: 'Chat',
-          url: 'https://gitter.im/vue-mdc-adapter/Lobby'
-        }
-      ],
-      mdcLinks: [
-        {
-          title: 'Documentation',
-          url: 'https://material.io/components/web/'
-        },
-        {
-          title: 'GitHub',
-          url: 'https://github.com/material-components/material-components-web'
-        },
-        {
-          title: 'Guidelines',
-          url: 'https://material.io/guidelines'
-        },
-        {
-          title: 'Awesome Material Components',
-          url: 'https://github.com/webdenim/awesome-material-components'
-        }
-      ]
-    }
-  },
-  components: { HelloWorld }
+    return {}
+  }
 }
 </script>
 
 <style lang="scss">
+  $mdc-theme-primary: #212121;
+  $mdc-theme-accent: #41b883;
+  $mdc-theme-background: #fff;
+
+  @import 'vue-mdc-adapter/dist/styles';
+
   // First, set the value for variable
   $mdc-typography-font-family: "Roboto Mono", monospace;
 
---
+++ src/views/internal/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <mdc-layout-app>
+
+    <mdc-top-app-bar slot="toolbar" title="GA Spendenquittungen" event="toggle-drawer">
+      <mdc-top-app-bar-action icon="help"></mdc-top-app-bar-action>
+    </mdc-top-app-bar>
+
+    <mdc-drawer slot="drawer" toggle-on="toggle-drawer" temporary>
+      <mdc-drawer-list>
+        <mdc-drawer-item start-icon="people">Spender</mdc-drawer-item>
+        <mdc-drawer-item start-icon="settings">Einstellungen</mdc-drawer-item>
+      </mdc-drawer-list>
+    </mdc-drawer>
+
+    <main>
+
+    </main>
+
+  </mdc-layout-app>
+</template>
+
+<script>
+export default {
+  name: 'Internal',
+  props: {
+  }
+}
+</script>
+
+<style lang="sass">
+
+</style>
