3b86e696bf4ceebf7e368e5196da466c02b2e18e
Kai Wittmann <k.wittmann@cmp-germany.com>
2018-12-28 21:20:53 +0100

    add material

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMDCAdapter from 'vue-mdc-adapter'
+
+Vue.use(VueMDCAdapter)
 
 Vue.config.productionTip = false
 
