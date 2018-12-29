babc642fdbe5e0aeb3dbfc1f7bc7ec2473bc390c
Kai Wittmann <k.wittmann@cmp-germany.com>
2018-12-29 16:40:57 +0100

    working on: create Verein card

--- src/views/internal/Settings/SettingsOverview.vue
+++ src/views/internal/Settings/SettingsOverview.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="settings-overview">
-    <mdc-layout-grid>
-      <mdc-layout-cell span=12 >
-        <mdc-display>Einstellungen</mdc-display>
-      </mdc-layout-cell>
-      <mdc-layout-cell desktop=6 tablet=6 phone=12 >
-        <mdc-headline>Verein</mdc-headline>
-      </mdc-layout-cell>
-      <mdc-layout-cell desktop=6 tablet=6 phone=12 >
-        <mdc-headline>Allgemein</mdc-headline>
-      </mdc-layout-cell>
-    </mdc-layout-grid>
-  </div>
+  <mdc-layout-grid>
+    <mdc-layout-cell span=12 >
+      <mdc-display>Einstellungen</mdc-display>
+    </mdc-layout-cell>
+    <mdc-layout-cell desktop=6 tablet=6 phone=12 >
+      <mdc-headline>Verein</mdc-headline>
+      <settings-overview-card
+        title="Generation Alive e.V."
+        subtitle="Eisenbahnstraße 23, 51645 Gummersbach"
+        :purposes="['123', '234', '345']"
+      />
+    </mdc-layout-cell>
+    <mdc-layout-cell desktop=6 tablet=6 phone=12 >
+      <mdc-headline>Allgemein</mdc-headline>
+    </mdc-layout-cell>
+  </mdc-layout-grid>
 </template>
 
 <script>
-export default {
+import SettingsOverviewCard from './SettingsOverviewCard'
 
+export default {
+  components: {
+    SettingsOverviewCard
+  }
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 
 </style>
---
+++ src/views/internal/Settings/SettingsOverviewCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <mdc-card>
+    <mdc-card-header v-if="title || subtitle"
+      :title="title"
+      :subtitle="subtitle" >
+    </mdc-card-header>
+    <mdc-card-text v-if="purposes && purposes.length">
+      <mdc-list>
+        <mdc-list-item v-for="(purpose, index) in purposes" :key="index">
+          <mdc-icon slot="start-detail" icon="flag" />
+          {{purpose}}
+        </mdc-list-item>
+      </mdc-list>
+    </mdc-card-text>
+    <mdc-card-text>
+      <mdc-list two-line>
+        <mdc-list-item>
+          <span>Gummersbach</span>
+          <span slot="secondary">Finanzamt</span>
+        </mdc-list-item>
+        <mdc-list-item>
+          <span>212/5825/2063</span>
+          <span slot="secondary">Steuer Nummer</span>
+        </mdc-list-item>
+      </mdc-list>
+    </mdc-card-text>
+    <mdc-card-actions>
+      <mdc-card-action-icons>
+        <mdc-card-action-icon icon="edit" />
+      </mdc-card-action-icons>
+    </mdc-card-actions>
+  </mdc-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    purposes: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
