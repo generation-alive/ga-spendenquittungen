b723286cf90f430426fd4edf784e20e267c70bcf
Kai Wittmann <k.wittmann@cmp-germany.com>
2018-12-29 16:00:49 +0100

    create layout

--- src/views/internal/Settings/SettingsOverview.vue
+++ src/views/internal/Settings/SettingsOverview.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Settings Overview</div>
+  <div class="settings-overview">
+    <mdc-layout-grid>
+      <mdc-layout-cell span=12 >
+        <mdc-display>Einstellungen</mdc-display>
+      </mdc-layout-cell>
+      <mdc-layout-cell desktop=6 tablet=6 phone=12 >
+        <mdc-headline>Verein</mdc-headline>
+      </mdc-layout-cell>
+      <mdc-layout-cell desktop=6 tablet=6 phone=12 >
+        <mdc-headline>Allgemein</mdc-headline>
+      </mdc-layout-cell>
+    </mdc-layout-grid>
+  </div>
 </template>
 
 <script>
