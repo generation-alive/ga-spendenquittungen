f3264d0df26c5d7a7415b70d043ef224debec2d7
Kai Wittmann <k.wittmann@cmp-germany.com>
2018-12-30 01:31:53 +0100

    create fields for general data

--- src/views/internal/Settings/SettingsEditOrganization.vue
+++ src/views/internal/Settings/SettingsEditOrganization.vue
@@ -1,10 +1,29 @@
 <template>
   <mdc-layout-grid>
     <mdc-layout-cell span=12 >
-      <mdc-display>Einstellungen</mdc-display>
+      <mdc-display>Bearbeiten: Verein</mdc-display>
     </mdc-layout-cell>
-    <mdc-layout-cell desktop=6 tablet=6 phone=12 >
-      <mdc-headline>Verein</mdc-headline>
+    <mdc-layout-cell desktop=6 tablet=6 phone=12>
+      <div class="icon-row">
+        <mdc-icon icon="person" class="icon-row__icon"/>
+        <div class="icon-row__content">
+          <mdc-textfield v-model="name" label="Name" outline class="full"/>
+        </div>
+      </div>
+      <div class="icon-row">
+        <mdc-icon icon="person_pin_circle" class="icon-row__icon"/>
+        <div class="icon-row__content">
+          <mdc-textfield v-model="street" label="Straße" outline class="two-third"/>
+          <mdc-textfield v-model="houseNr" label="Nummer" outline class="one-third"/>
+        </div>
+      </div>
+      <div class="icon-row">
+        <mdc-icon class="icon-row__icon"/>
+        <div class="icon-row__content">
+          <mdc-textfield v-model="zip" label="PLZ" outline class="one-third"/>
+          <mdc-textfield v-model="city" label="Ort" outline class="two-third"/>
+        </div>
+      </div>
     </mdc-layout-cell>
   </mdc-layout-grid>
 </template>
@@ -14,6 +33,15 @@ import Organization from '@/store/models/Organization'
 
 export default {
   name: 'SettingsEditOrganization',
+  data () {
+    return {
+      name: '',
+      street: '',
+      houseNr: '',
+      zip: '',
+      city: ''
+    }
+  },
   computed: {
     organization () {
       return Organization.query().with('address').with('purpose').find(1)
@@ -41,5 +69,27 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+  .icon-row
+    display: flex
 
+    &__icon
+      align-self: center
+      width: 35px
+      margin: .7rem 1rem .5rem
+      flex-grow: 0
+      flex-shrink: 0
+      font-size: 35px
+
+    &__content
+      flex-grow: 1
+      flex-shrink: 1
+      display: flex
+      justify-content: space-between
+
+  .full
+    width: 100%
+  .two-third
+    width: 65%
+  .one-third
+    width: 30%
 </style>
