a2dc6b0952d374d60ad0202fed3f22a45d088533
Kai Wittmann <k.wittmann@cmp-germany.com>
2019-01-04 17:47:12 +0100

    working on: create view

--- src/views/internal/Donators/DonatorsOverview.vue
+++ src/views/internal/Donators/DonatorsOverview.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Donators Overview</div>
+  <mdc-layout-grid>
+    <mdc-layout-cell span=12 >
+      <mdc-display>Spender</mdc-display>
+    </mdc-layout-cell>
+    <mdc-layout-cell desktop=6 tablet=6 phone=12 >
+      <mdc-list>
+        <mdc-list-item>Single-line item</mdc-list-item>
+        <mdc-list-item>Single-line item</mdc-list-item>
+        <mdc-list-item>Single-line item</mdc-list-item>
+      </mdc-list>
+    </mdc-layout-cell>
+  </mdc-layout-grid>
 </template>
 
 <script>
