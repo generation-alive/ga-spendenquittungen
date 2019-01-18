06f0f1b906d77d42fb66e613fae72cb52e3bba38
Kai Wittmann <k.wittmann@cmp-germany.com>
2019-01-18 23:36:55 +0100

    install persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VuexORM from '@vuex-orm/core'
+import createPersistedState from 'vuex-persistedstate'
 
 // Models
 import Address from './models/Address'
@@ -34,7 +35,10 @@ database.register(Donator, donators)
 
 // Create Vuex Store and register database through Vuex ORM.
 const store = new Vuex.Store({
-  plugins: [VuexORM.install(database)]
+  plugins: [
+    VuexORM.install(database),
+    createPersistedState()
+  ]
 })
 
 export default store
