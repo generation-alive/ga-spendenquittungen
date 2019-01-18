5e3faea69d993055ab300e1fcc92a5969a10cc77
Kai Wittmann <k.wittmann@cmp-germany.com>
2019-01-18 20:54:02 +0100

    import names

--- src/helpers/importDatabase.js
+++ src/helpers/importDatabase.js
@@ -1,3 +1,10 @@
-export default (importData) => {
+import Donator from '@/store/models/Donator'
+import _ from 'lodash'
 
+export default (persons) => {
+  let processedData = _.map(persons, (person) => ({
+    id: person._id,
+    name: `${person.first_name} ${person.last_name}`
+  }))
+  Donator.insert({ data: processedData })
 }
